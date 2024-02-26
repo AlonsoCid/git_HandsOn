3a9d1eca3eaac815ecce26e7a69a94a8f980432c
Alonso <alonsocidpadilla8@gmail.com>
2024-02-26 20:33:38 +0100

    fix classification

--- seqClass.py
+++ seqClass.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Input motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -12,12 +13,21 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+args.seq = args.seq.upper() 
+if re.search('^[ACGUT]+$', args.seq): 
+    if 'T' in args.seq and 'U' in args.seq:  # Added condition to check for the presence of both 'T' and 'U' in the sequence ensuring base exclusivity
+        print ('The sequence contains both DNA and RNA bases')
+    elif 'T' in args.seq:
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif 'U' in args.seq:
         print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
